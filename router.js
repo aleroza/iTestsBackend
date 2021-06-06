e88efc118f3022ebf06f8cf69c68818d21e05a66
aleroza <aleroza1910@gmail.com>
2021-06-06 15:09:46 +0300

    Pre-Diploma

--- router.js
+++ router.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 let Test = require('./test.model');
+const mongoose = require("mongoose");
 
 /*
  *TODO ФУНКЦИЯ ПРОВЕРКИ ВАЛИДНОСТИ ПАРАМЕТРОВ
  * https://github.com/VKCOM/vk-apps-launch-params
+ */
+
+/*
+ *FIXME переименовать получаемые значения в единый стиль (сейчас есть и testID, и _id)
  */
 
 // router.route('/test_init').post((req, res) => {
@@ -62,32 +67,32 @@ router.route('/set_score').patch((req, res) => {
 router.route('/tests_list_admin').post((req, res) => {
     {
         //security test
-        Test.aggregate([
-            {
-                $match: {
-                    ownerID: req.body.ownerID,
-                    isGroup: req.body.isGroup,
+            Test.aggregate([
+                {
+                    $match: {
+                        ownerID: req.body.ownerID,
+                        isGroup: req.body.isGroup,
+                    }
                 }
-            }
-            , {
-                $project: {
-                    title: 1,
-                    description: 1,
-                    image: 1,
-                    isActive: 1,
-                    showAnswers: 1,
-                    numberOfAttempts: 1,
-                    numberOfQuestions: {
-                        $size: "$questions"
-                    },
-                    numberOfResults: {
-                        $size: "$results"
-                    },
-                    createdAt: 1,
-                    updatedAt: 1
+                , {
+                    $project: {
+                        title: 1,
+                        description: 1,
+                        image: 1,
+                        isActive: 1,
+                        showAnswers: 1,
+                        numberOfAttempts: 1,
+                        numberOfQuestions: {
+                            $size: "$questions"
+                        },
+                        numberOfResults: {
+                            $size: "$results"
+                        },
+                        createdAt: 1,
+                        updatedAt: 1
+                    }
                 }
-            }
-        ])
+            ])
             .then(tests => {
                 res.json(tests)
                 console.log(`ADMIN: Tests list id${req.body.ownerID}-${req.params.isGroup ? "group" : "user"}`)
@@ -97,6 +102,19 @@ router.route('/tests_list_admin').post((req, res) => {
                 console.log('Error: ' + err)
             });
     }
+})
+
+router.route('/get_test_data_admin').post((req, res) => {
+    //security
+    Test.findById(req.body.testID, {questions:1,finals:1,results:1})
+        .then(testData => {
+            res.json(testData)
+            console.log(`ADMIN: Tests #${req.body.testID}`)
+        })
+        .catch(err => {
+            res.status(400).json('Error: ' + err)
+            console.log('Error: ' + err)
+        });
 })
 
 router.route('/tests_list/:ownerID/:isGroup/:userID').get((req, res) => {
@@ -154,6 +172,67 @@ router.route('/delete_test').delete((req, res) => {
         });
 })
 
+router.route('/get_results').post((req, res) => {
+    Test.aggregate([{
+        $match: {
+            _id: mongoose.Types.ObjectId(req.body._id)
+        }
+    }, {
+        $project: {
+            results: {
+                $filter: {
+                    input: '$results',
+                    as: 'item',
+                    cond: {
+                        $ne: [
+                            '$$item.score', (req.body.zeroes?-1:0)
+                        ]
+                    }
+                }
+            }
+        }
+    }, {
+        $unwind: {
+            path: "$results"
+        }
+    }, {
+        $project: {
+            'results.attemptsLeft': 0
+        }
+    }, {
+        $sort: {
+            'results.score': -1
+        }
+    }, {
+        $group: {
+            _id: "$_id",
+            results: {
+                $push: "$results"
+            }
+        }
+    }])
+        .then((dbres) => {
+        res.json(dbres[0])
+        console.log('Sent test results: ' + req.body._id)
+    })
+        .catch(err => {
+            res.status(400).json('Error: ' + err)
+            console.log('Error: ' + err)
+        });
+})
+
+router.route('/delete_results').delete((req, res) => {
+    Test.findByIdAndUpdate(req.body._id, {results: []})
+        .then((dbres) => {
+            res.json("Results deleted!")
+            console.log('Deleted results in test: ' + req.body._id)
+        })
+        .catch(err => {
+            res.status(400).json('Error: ' + err)
+            console.log('Error: ' + err)
+        });
+})
+
 router.route('/test/:testID/:userID').get((req, res) => {
     console.log(req.params)
     Test.findById(req.params.testID, {
@@ -181,9 +260,26 @@ router.route('/add').post((req, res) => {
     const newTest = new Test(req.body)
 
     newTest.save()
-        .then((dbres) => console.log('New test in database: ' + dbres._id))
-        .then(() => res.json("Test added!"))
-        .catch(err => res.status(400).json('Error: ' + err));
+        .then((dbres) => {
+            res.json("Test added!")
+            console.log('New test in database: ' + dbres._id)
+        })
+        .catch(err => {
+            res.status(400).json('Error: ' + err)
+            console.log('Error: ' + err)
+        });
+});
+
+router.route('/update').post((req, res) => {
+    Test.findByIdAndUpdate(req.body._id, req.body)
+        .then((dbres) => {
+            res.json("Test updated!")
+            console.log('Updated test in database: ' + dbres._id)
+        })
+        .catch(err => {
+            res.status(400).json('Error: ' + err)
+            console.log('Error: ' + err)
+        });
 });
 
 
